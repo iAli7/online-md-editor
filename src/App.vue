57e02e2a8a65675e41e593ef15aecf9c2a8aadc1
iAli7 <aligokyer206@gmail.com>
2023-07-31 03:15:45 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <editor :editor-text="'sa'" />
+</template>
+
+<script setup lang="ts">
+import editor from './components/editor/editor.vue';
+</script>
---
+++ src/components/editor/editor.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card">
+        <textarea class="card-content" :value="editorText"></textarea>
+    </div>
+</template>
+
+<style src="./editor.scss" lang="sass"></style>
+
+<script setup lang="ts">
+defineProps(['editorText'])
+</script>
